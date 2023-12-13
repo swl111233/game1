8cfd45c59de5f64b88c0e904cadbc77b289e811b
bkgoo <877355834@qq.com>
2023-12-13 20:27:11 +0800

    吕磊 碰撞宝石触发多子弹技能

--- Assets/Swl/Scripts/GameController111.cs
+++ Assets/Swl/Scripts/GameController111.cs
@@ -1,55 +1,77 @@
 using System.Collections;
+using System.Collections.Generic;
 using UnityEngine;
 using UnityEngine.UI;
+using UnityEngine.SceneManagement;
 
 public class GameController111 : MonoBehaviour
 {
-    public GameObject hazard;
+    //hazards、hazardCounts、spawnRates的数组数量要一一对应
+    //随机出现的对象
+    public GameObject[] hazards;
+    //出现的频率，在0-1之间，越大代表生成频率越低
+    public float[] spawnRates;
     public Vector3 spawnValues;
-    public int hazardCount;
+    //出现的个数
+    public int[] hazardCounts;
+    //生成间隔时间
     public float spawnWait;
+    //开始生成时间
     public float startWait;
+    //每一波的间隔时间
     public float waveWait;
+
     public Text scoreText;
     private int score;
+
     public Text gameOverText;
     private bool gameOver;
+
+    public Text restartText;
+    private bool restart;
 
     IEnumerator SpawnWaves()
     {
         yield return new WaitForSeconds(startWait);
 
+        int hazardCount = 0;//统计障碍物出现的个数
+        int gemInterval = Random.Range(12, 16); // 宝石出现的间隔随机设置为12到16个障碍物之后
+
         while (true)
         {
-
-            for (int i = 0; i < hazardCount; i++)
+            for (int i = 0; i < hazards.Length; i++)//遍历所有所要生成的对象类型，方便后续更多对象随机生成
             {
-                Vector3 spawnPosition = new Vector3(Random.Range(-spawnValues.x, spawnValues.x), spawnValues.y, spawnValues.z);
-                Quaternion spawnRotation = Quaternion.identity;
-                Instantiate(hazard, spawnPosition, spawnRotation);
-                yield return new WaitForSeconds(spawnWait);
+                int count = hazardCounts[i];
+                for (int j = 0; j < count; j++) //生成指定数量
+                {
+                    Vector3 spawnPosition = new Vector3(Random.Range(-spawnValues.x, spawnValues.x), spawnValues.y, spawnValues.z);
+                    Quaternion spawnRotation = Quaternion.identity;
+                    Instantiate(hazards[i], spawnPosition, spawnRotation);
+
+                    hazardCount++;
+
+                    if (hazardCount % gemInterval == 0)// 如果已生成的障碍物数量达到了宝石间隔
+                    {
+                        Vector3 gemSpawnPosition = new Vector3(Random.Range(-spawnValues.x, spawnValues.x), spawnValues.y, spawnValues.z);
+                        Quaternion gemSpawnRotation = Quaternion.identity;
+                        Instantiate(hazards[1], gemSpawnPosition, gemSpawnRotation);// 在指定位置生成宝石
+                        gemInterval = Random.Range(12, 16); // 每次生成宝石后重新设置宝石出现的间隔
+                    }
+
+                    float spawnRate = spawnRates[i];
+                    yield return new WaitForSeconds(spawnRate);
+                }
             }
+
             yield return new WaitForSeconds(waveWait);
 
             if (gameOver)
             {
+                restartText.text = "点击【R】重新开始";
+                restart = true;
                 break;
             }
         }
-    }
-
-    // Start is called before the first frame update
-    void UpdateScore()
-    {
-        scoreText.text = "分数：" + score;
-    }
-    void Start()
-    {
-        score = 0;
-        StartCoroutine(SpawnWaves());
-        UpdateScore();
-        gameOverText.text = "";
-        gameOver = false;
     }
 
     public void AddScore(int newScoreValue)
@@ -57,6 +79,35 @@ public class GameController111 : MonoBehaviour
         score += newScoreValue;
         UpdateScore();
     }
+
+    void UpdateScore()
+    {
+        scoreText.text = "分数：" + score;
+    }
+
+    void Start()
+    {
+        score = 0;
+        UpdateScore();
+        StartCoroutine(SpawnWaves());
+        gameOverText.text = "";
+        gameOver = false;
+        restartText.text = "";
+        restart = false;
+    }
+
+    void Update()
+    {
+        if (restart)
+        {
+            if (Input.GetKeyDown(KeyCode.R))
+            {
+                SceneManager.LoadScene(SceneManager.GetActiveScene().buildIndex);
+                restart = false;
+            }
+        }
+    }
+
     public void GameOver()
     {
         gameOverText.text = "游戏结束";
--- Assets/Swl/Scripts/PlayerControl.cs
+++ Assets/Swl/Scripts/PlayerControl.cs
@@ -1,4 +1,7 @@
 using UnityEngine;
+using System.Collections;
+using UnityEngine.UI;
+using System.Collections.Generic;
 
 public class PlayerControl : MonoBehaviour
 {
@@ -7,6 +10,7 @@ public class PlayerControl : MonoBehaviour
     {
         public float xMin, xMax, zMin, zMax;
     }
+
     public float speed = 5.0f;
     public Boundary boundary;
     public float tilt = 4.0f;
@@ -16,37 +20,96 @@ public class PlayerControl : MonoBehaviour
     public float fireRate = 0.5f;
     private float nextFire;
 
-    // Start is called before the first frame update
-    void Start()
-    {
+    // 技能一：多发子弹
+    private bool isUsingSkill = false;
+    //技能持续时间
+    public float skillDuration = 5.0f;
+    private float skillTime;
+    public Slider timerSlider;
+    //子弹数量
+    public int bulletNum = 3;
+    public float bulletAngle = 15;
 
+
+    private void Start()
+    {
+        skillTime = skillDuration;
+        timerSlider.gameObject.SetActive(false);
     }
 
-    // Update is called once per frame
-    void Update()
+    private void Update()
     {
         if (Input.GetButton("Fire1") && Time.time > nextFire)
         {
             nextFire = Time.time + fireRate;
-            Instantiate(shot, shotSpawn.position, shotSpawn.rotation);
+            Instantiate(shot, shotSpawn.position, shotSpawn.rotation);//创建出子弹
             GetComponent<AudioSource>().Play();
         }
+
+        //多发子弹技能状态判断
+        if (isUsingSkill)
+        {
+            timerSlider.gameObject.SetActive(true);
+            skillTime -= Time.deltaTime;
+            if (skillTime <= 0)
+            {
+                Debug.Log("Countdown finished!");
+                EndTripleSkill();
+            }
+            else
+            {
+                timerSlider.value = skillTime / skillDuration;
+                TripleSkill();
+            }
+        }
     }
-    private void FixedUpdate()
+
+    void FixedUpdate()
     {
+        //获取横向纵向输入值
         float moveHorizontal = Input.GetAxis("Horizontal");
         float moveVertical = Input.GetAxis("Vertical");
 
         Vector3 movement = new Vector3(moveHorizontal, 0.0f, moveVertical);
         GetComponent<Rigidbody>().velocity = movement * speed;
-
         GetComponent<Rigidbody>().position = new Vector3
             (
             Mathf.Clamp(GetComponent<Rigidbody>().position.x, boundary.xMin, boundary.xMax),
             0.0f,
             Mathf.Clamp(GetComponent<Rigidbody>().position.z, boundary.zMin, boundary.zMax)
             );
+
         GetComponent<Rigidbody>().rotation =
-        Quaternion.Euler(0.0f, 0.0f, GetComponent<Rigidbody>().velocity.x * -tilt);
+            Quaternion.Euler(0.0f, 0.0f, GetComponent<Rigidbody>().velocity.x * -tilt);
+    }
+
+    //识别是否是Triple Stone
+    private void OnTriggerEnter(Collider other)
+    {
+        if (other.CompareTag("TripleStone"))
+        {
+            isUsingSkill = true;
+            Destroy(other.gameObject);
+        }
+    }
+
+    //多发子弹技能实现
+    private void TripleSkill()
+    {
+        int median = bulletNum / 2;
+        for (int i = 0; i < bulletNum; i++)
+        {
+            // 实例化子弹对象并设置速度
+            GameObject newBullet = Instantiate(shot, shotSpawn.position, Quaternion.identity);
+            newBullet.transform.Rotate(Vector3.up, bulletAngle * (i - median));
+        }
+    }
+
+    //多发子弹技能结束
+    private void EndTripleSkill()
+    {
+        isUsingSkill = false;
+        skillTime = skillDuration;
+        timerSlider.gameObject.SetActive(false);
     }
 }
